5f691a195e815475f8754283e8f0fb07d146ffbd
RituSolanki29 <ritusolanki1629@example.com>
2026-01-10 13:17:52 +0530

    Fix

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,12 +1,12 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "./config/cloudinary.js";
+import cloudinary from "../config/cloudinary.js"; 
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "vyom",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
